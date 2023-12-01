994d48bb2caee99e90c5a5fd3c3dea0117cb2efe
AvinashKumar0923 <71658168+AvinashKumar0923@users.noreply.github.com>
2023-12-02 02:20:58 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,114 @@
+// local imports
+import shippingIcon from "./assets/free-shiping-icon.svg";
+import paymentIcon from "./assets/quick-payment-icon.svg";
+import supportIcon from "./assets/support-icon.svg";
+import productOneIcon from "./assets/product-one.svg";
+import productTwoIcon from "./assets/produc-two.svg";
+import productThreeIcon from "./assets/product-three.svg";
+import plantIcon from "./assets/plant-icon.svg";
+import sunIcon from "./assets/sun-icon.svg";
+import waterIcon from "./assets/water-icon.svg";
+import temperatureIcon from "./assets/temperature-icon.svg";
+
+export const heroTitle = "Infuse Tranquility into Your Space with Indoor Greenery";
+export const heroSubtitle =
+  "Discover the Botanical Jewel for Your Home Decor with Us, and Watch Your Dream Oasis Blossom.";
+
+export const services = [
+  {
+    title: "Free Shipping",
+    subtitle: "No charge for your delivery",
+    icon: shippingIcon,
+  },
+  {
+    title: "Quick Payment",
+    subtitle: "100% secure",
+    icon: paymentIcon,
+  },
+  {
+    title: "24/7 Support",
+    subtitle: "Don't hesitate to contact us",
+    icon: supportIcon,
+  },
+];
+
+export const productsTitle = "Best Selling Products";
+export const productsBtnText = "See Colection";
+
+export const products = [
+  {
+    title: "Golden Pothos",
+    price: "$69.99",
+    img: productOneIcon,
+  },
+  {
+    title: "Rubber Plant",
+    price: "$79.00",
+    img: productTwoIcon,
+  },
+  {
+    title: "Cast Iron Plant",
+    price: "$86.50",
+    img: productThreeIcon,
+  },
+];
+
+export const referenceTitle = "Greenery Inspirations for Stylish Interiors";
+export const referenceSubtitle = "Transform Your Living Space into a Cozy Haven with the Invigorating Presence of Indoor Plants.";
+
+export const careTitle = "How to care for plants";
+export const careSubtitle = "Take care of plants with all your heart";
+
+export const careList = [
+  {
+    title: "Adjust Lighting",
+    subtitle:
+      "When caring for houseplants, make sure the room temperature is neither too cold nor too hot.",
+    img: sunIcon,
+  },
+  {
+    title: "Don't water too often",
+    subtitle:
+      "Watering ornamental houseplants does not have to be done every day.",
+    img: waterIcon,
+  },
+  {
+    title: "Choose the right container.",
+    subtitle:
+      "Houseplants will need different containers depending on their type and size.",
+    img: plantIcon,
+  },
+  {
+    title: "Fertilize regularly",
+    subtitle:
+      "The nutrients most indoor houseplants need are nitrogen for balance and potassium for stem strength.",
+    img: temperatureIcon,
+  },
+];
+
+export const newsletter = "Newsletter";
+
+export const footerLinksColumnOne = [
+  {
+    title: "Support",
+    links: ["About Us", "Careers", "Blog"],
+  },
+  {
+    title: "Useful Links",
+    links: ["Payment & Tax", "Terms of service", "Privacy Policy"],
+  },
+];
+
+export const footerLinksColumnTwo = [
+  {
+    title: "Our Menu",
+    links: ["Best Product", "Categories"],
+  },
+  {
+    title: "Address",
+    links: [
+      "Chitkara University, Rajpura, Punjab",
+      "findplants@decor.com",
+    ],
+  },
+];
